8583ebed53e1bd5eae4e0778e02f20b061f5654c
Hypermona <mohans8050@gmail.com>
2021-03-23 22:43:09 +0530

    added analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Main from "./Components/main";
 import { Provider } from "react-redux";
 import { ConfigureStore } from "./Redux/configureStore";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga";
 
 const store = ConfigureStore();
-
+useEffect(() => {
+  ReactGA.initialize("UA-192838886-1");
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}, []);
 function App() {
   return (
     <Provider store={store}>
